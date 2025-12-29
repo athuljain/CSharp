e57267ca8bb4e931eceabd977f7842af1ecf9dc9
athuljain <athuljainkj2@gmail.com>
2025-12-29 14:57:42 +0530

    C# operators

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,3 +4,16 @@ Console.WriteLine(10+25);
 
 Console.Write("hello");
 Console.Write("World");
+
+
+//  operators
+
+
+int a=10;
+int b=15;
+
+Console.WriteLine(a+b);
+Console.WriteLine(a-b);
+Console.WriteLine(a*b);
+Console.WriteLine(a/b);
+Console.WriteLine(a%b);
