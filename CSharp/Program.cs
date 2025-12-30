2b1cb601548c19ceb490e8945e2a4b158ac4cb74
athuljain <athuljainkj2@gmail.com>
2025-12-30 08:49:52 +0530

    user input and opertaions

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,19 +1,49 @@
 ﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine(10+25);
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine(10+25);
 
-Console.Write("hello");
-Console.Write("World");
+// Console.Write("hello");
+// Console.Write("World");
 
 
 //  operators
 
 
-int a=10;
-int b=15;
+// int a=10;
+// int b=15;
 
-Console.WriteLine(a+b);
-Console.WriteLine(a-b);
-Console.WriteLine(a*b);
-Console.WriteLine(a/b);
-Console.WriteLine(a%b);
+// double a=10;
+// double b=15;
+
+// Console.WriteLine(a+b);
+// Console.WriteLine(a-b);
+// Console.WriteLine(a*b);
+// Console.WriteLine(a/b);
+// Console.WriteLine(a%b);
+
+
+// user input
+
+// Console.WriteLine("enter a name");
+// // String name=Console.ReadLine();
+// String? name=Console.ReadLine();
+
+// Console.WriteLine("your name is "+name);
+
+
+// Console.WriteLine("enter a number");
+// int number=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Your number is :"+number);
+
+
+
+Console.WriteLine("enter first number");
+int number1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter second number");
+int number2=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("sum of " + number1 + " and " + number2 + " is " + (number1 + number2));
+
+
+Console.WriteLine($"sum of {number1} and {number2} is {number1 + number2}");
+
+
--- CSharp/obj/Debug/net10.0/CSharp.AssemblyInfo.cs
+++ CSharp/obj/Debug/net10.0/CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07c13a7d4d1537660b457e91cb5f5ec2605f9df1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57267ca8bb4e931eceabd977f7842af1ecf9dc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
