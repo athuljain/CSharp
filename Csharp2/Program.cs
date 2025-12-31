ca2525de983eae1ec06868b22f289b6e752ebaec
athuljain <athuljainkj2@gmail.com>
2025-12-31 16:05:37 +0530

    c# basicss

---
+++ Csharp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
