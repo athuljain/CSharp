1145d63a23f716d52682aa66dfd267479df76e77
athuljain <athuljainkj2@gmail.com>
2026-01-08 13:18:21 +0530

    advance

--- Csharp3/Program.cs
+++ Csharp3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// ========================================
+
+// inheritance
+
+
+
+
+
+// namespace Program
+// {
+//     class Animal
+//     {
+//         public void Eat()
+//         {
+//             Console.WriteLine("Animal is Eating");
+//         }
+//     }
+//     class Dog : Animal
+//     {
+//         public void Bark()
+//         {
+//             Console.WriteLine("Dog is barking");
+//         }
+//     }
+
+//    public class Program
+//     {
+//       public  static void Main()
+//         {
+//             Dog d=new Dog();
+//             d.Eat();
+//             d.Bark();
+//         }
+//     }
+// }
